3e52b51ed8d186bf1d22b1cbf97196f0c17cde99
Joseph Aidoo <joseph.aidoo@rancard.com>
2025-05-06 16:10:34 +0000

    addition of additional logic

--- src/testing_protected_field/DataScience.java
+++ src/testing_protected_field/DataScience.java
@@ -1,8 +1,7 @@
 package testing_protected_field;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.Flow;
 
 
 record  students(String name, int age){
@@ -14,6 +13,48 @@ public class DataScience {
         var student = new students("Joseph", 20);
         System.out.println(student.name());
         System.out.println(student.age());
+
+    }
+
+
+    public boolean checkRecord(String s) {
+        Map<Character, Integer> characterIntegerMap = new HashMap<>();
+
+        List<Character> list = List.of('A', 'L', 'P');
+        boolean isTrue = false;
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(!list.contains(c)){
+                continue;
+
+            }
+
+            if(c == 'A'){
+                int count = characterIntegerMap.getOrDefault('A', 0);
+                if(count < 2)  isTrue = true;
+                characterIntegerMap.put('A', count + 1);
+
+            }else if(c == 'L'){
+
+
+
+            }else{
+                characterIntegerMap.put(c, characterIntegerMap.getOrDefault(c, 0) + 1);
+            }
+
+
+
+
+
+        }
+
+        return characterIntegerMap.getOrDefault('A', 0) <= 1 && characterIntegerMap.getOrDefault('L', 0) <= 2 && characterIntegerMap.getOrDefault('P', 0) <= 2;
+
+
+
+
 
     }
 
@@ -57,6 +98,9 @@ public class DataScience {
     }
 
 
+
+
+
     private String reverseVowels2(String string){
 
 
@@ -94,4 +138,61 @@ public class DataScience {
     FirstClass firstClass = new FirstClass("Joseph", 20);
     String description = firstClass.description;
     String name = firstClass.name;
+}
+
+
+
+class GenericBox<T, V>{
+
+    private T value;
+
+    private V value2;
+
+
+    public GenericBox(T value){
+        this.value = value;
+    }
+
+    public T getValue(){
+        return value;
+    }
+    public void setValue(T value){
+        this.value = value;
+    }
+
+
+}
+
+
+class TestingGenericConcept{
+
+    public static void main(String[] args) {
+
+        GenericBox<String, String> genericBox = new GenericBox<>("Paul");
+        System.out.println(genericBox.getValue());
+        genericBox.setValue("Joseph");
+        System.out.println(genericBox.getValue());
+        System.out.println(genericBox.getValue().getClass());
+
+        System.out.println(max("Joseph", "Joseph"));
+
+        String value = max("Ben", "Simon");
+        System.out.println(value);
+        System.out.println(value.getClass());
+        System.out.println(value.getClass().getTypeName());
+
+
+
+
+
+    }
+
+
+
+    public static <T, V>  V max(T a, V b){
+
+        return b;
+    }
+
+
 }
